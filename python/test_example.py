bc5e8ac08e4d83b1f4926e1f2589f62de05639cc
undredal <undredal@fake.com>
2015-06-06 11:58:34 +0600

    2

--- python/test_example.py
+++ python/test_example.py
@@ -16,24 +16,76 @@ def check_records_count(driver, ass, value, num):
     ass(len(product_items), num)
 
 class IdTest(BaseTestCase):
-   def test_find_1(self):
-       check_records_count(self.driver, self.assertEqual, 1, 1)
-   def test_find_2(self):
-       check_records_count(self.driver, self.assertEqual, 2, 1)
-   def test_find_3(self):
-       check_records_count(self.driver, self.assertEqual, 3, 1)
-   def test_find_4(self):
-       check_records_count(self.driver, self.assertEqual, 4, 1)
-   def test_find_5(self):
-       check_records_count(self.driver, self.assertEqual, 5, 1)
-   def test_find_6(self):
-       check_records_count(self.driver, self.assertEqual, 6, 1)
-   def test_find_7(self):
-       check_records_count(self.driver, self.assertEqual, 7, 1)
-   def test_find_8(self):
-       check_records_count(self.driver, self.assertEqual, 8, 1)
-   def test_find_9(self):
+    def test_find_1(self):
+        check_records_count(self.driver, self.assertEqual, 1, 1)
+    def test_find_2(self):
+        check_records_count(self.driver, self.assertEqual, 2, 1)
+    def test_find_3(self):
+        check_records_count(self.driver, self.assertEqual, 3, 1)
+    def test_find_4(self):
+        check_records_count(self.driver, self.assertEqual, 4, 1)
+    def test_find_5(self):
+        check_records_count(self.driver, self.assertEqual, 5, 1)
+    def test_find_6(self):
+        check_records_count(self.driver, self.assertEqual, 6, 1)
+    def test_find_7(self):
+        check_records_count(self.driver, self.assertEqual, 7, 1)
+    def test_find_8(self):
+        check_records_count(self.driver, self.assertEqual, 8, 1)
+    def test_find_9(self):
         check_records_count(self.driver, self.assertEqual, 9, 0)
+
+class NameTest(BaseTestCase):
+    def test_find_ms(self):
+        check_records_count(self.driver, self.assertEqual, 'Монитор Sony', 1)
+    def test_find_mp(self):
+        check_records_count(self.driver, self.assertEqual, 'Монитор Panasonic', 1)
+    def test_find_md(self):
+        check_records_count(self.driver, self.assertEqual, 'Монитор DELL', 1)
+    def test_find_pe(self):
+        check_records_count(self.driver, self.assertEqual, 'Принтер Epson', 1)
+    def test_find_km(self):
+        check_records_count(self.driver, self.assertEqual, 'Клавиатура Microsoft', 1)
+    def test_find_kl(self):
+        check_records_count(self.driver, self.assertEqual, 'Клавиатура Logitec', 1)
+    def test_find_mm(self):
+        check_records_count(self.driver, self.assertEqual, 'Мышь Microsoft', 1)
+    def test_find_tc(self):
+        check_records_count(self.driver, self.assertEqual, 'Телефон Cisco', 1)
+    def test_find_not_found(self):
+        check_records_count(self.driver, self.assertEqual, 'Мой клёвый продукт', 0)
+
+class TypeTest(BaseTestCase):
+    def test_find_m(self):
+        check_records_count(self.driver, self.assertEqual, 'Монитор', 3)
+    def test_find_p(self):
+        check_records_count(self.driver, self.assertEqual, 'Принтер', 1)
+    def test_find_k(self):
+        check_records_count(self.driver, self.assertEqual, 'Клавиатура', 2)
+    def test_find_mouse(self):
+        check_records_count(self.driver, self.assertEqual, 'Мышь', 1)
+    def test_find_t(self):
+        check_records_count(self.driver, self.assertEqual, 'Телефон', 1)
+    def test_find_not_found(self):
+        check_records_count(self.driver, self.assertEqual, 'Мой клёвый продукт', 0)
+
+class VendorTest(BaseTestCase):
+    def test_find_s(self):
+        check_records_count(self.driver, self.assertEqual, 'Sony', 1)
+    def test_find_p(self):
+        check_records_count(self.driver, self.assertEqual, 'Panasonic', 1)
+    def test_find_d(self):
+        check_records_count(self.driver, self.assertEqual, 'DELL', 1)
+    def test_find_e(self):
+        check_records_count(self.driver, self.assertEqual, 'Epson', 1)
+    def test_find_m(self):
+        check_records_count(self.driver, self.assertEqual, 'Microsoft', 2)
+    def test_find_l(self):
+        check_records_count(self.driver, self.assertEqual, 'Logitec', 1)
+    def test_find_c(self):
+        check_records_count(self.driver, self.assertEqual, 'Cisco', 1)
+    def test_find_not_found(self):
+        check_records_count(self.driver, self.assertEqual, 'Мой клёвый продукт', 0)
 
 # class ExampleTest(BaseTestCase):
 #      def test_example(self):
